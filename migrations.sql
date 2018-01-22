f0f372ca3aab926d4014ebb395185f86235c998b
aurelienlair <aurelien.lair@onebip.com>
2018-01-22 18:31:15 +0100

    documentation: adding docs for Tmux and some useful Shell commands

--- migrations.sql
+++ migrations.sql
@@ -42,3 +42,47 @@ VALUES ('Git help with Fugitive', 'Command', 'git.html#git_help_fugitive');
 
 INSERT OR IGNORE INTO searchIndex(name, type, path) 
 VALUES ('Git cherry-pick', 'Command', 'git.html#git_cherry_pick');
+
+
+/* TMUX */
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux prompt mode', 'Command', 'tmux.html#control_space_colon');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux list shortcuts', 'Command', 'tmux.html#control_space_question_mark');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux paste buffer', 'Command', 'tmux.html#control_space_closing_bracket');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux copy mode', 'Command', 'tmux.html#control_space_opening_bracket');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux list buffers', 'Command', 'tmux.html#control_space_hash');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux move current window', 'Command', 'tmux.html#control_space_doth');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Tmux choose and paste buffer', 'Command', 'tmux.html#control_space_equal');
+
+
+/* SHELL*/
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Shell cut the word before the cursor', 'Command', 'shell.html#control_w');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Shell cut all the words before the cursor', 'Command', 'shell.html#control_u');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ("Shell yank buffer from the cursor's position", 'Command', 'shell.html#control_y');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Shell move back the cursor', 'Command', 'shell.html#alt_b');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Shell move forward the cursor', 'Command', 'shell.html#alt_f');
+
+INSERT OR IGNORE INTO searchIndex(name, type, path)
+VALUES ('Shell delete characters from current cursor', 'Command', 'shell.html#alt_d');
+
